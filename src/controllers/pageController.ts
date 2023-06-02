28d7ef8f33a46af37d596b68bf12a882d72e914c
Daniel mpm <dmarcosmpm.js@outlook.com>
2023-06-02 14:31:32 -0300

    Finaly Project -- version 1.0.0

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,18 +1,61 @@
-import { Request, Response } from 'express'
+import { Request, Response } from 'express';
+import { Pet } from '../models/pet';
+import { createMenuObject } from '../helpers/createMenuObjects';
 
-export const home = ((req: Request, res: Response) => {
-    // ►
-    res.send('HOME - CONTROLLER')
-})
+export const home = (req: Request, res: Response) => {
+    // ► Chamada da função de Models line: 135
+    let list = Pet.getAll();
 
-export const dogs = ((req: Request, res: Response) => {
-    // ►
-})
+    let time: number = new Date().getFullYear();
 
-export const cats = ((req: Request, res: Response) => {
-    // ►
-})
+    res.render('pages/page', {
+        menu: createMenuObject('all'),
+        banner: {
+            title: 'Todos animais do site',
+            background: 'allanimals.jpg',
+        },
+        list,
+        time
+    });
+};
 
-export const fishes = ((req: Request, res: Response) => {
-    // ►
-})
+export const dogs = (req: Request, res: Response) => {
+    // ► Filtragem → (Models)
+    let list = Pet.getFromType('dog');
+    res.render('pages/page', {
+        menu: createMenuObject('dog'),
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg',
+        },
+        list,
+    });
+};
+
+export const cats = (req: Request, res: Response) => {
+    // ► // ► Filtragem → (Models)
+    let list = Pet.getFromType('cat');
+
+    res.render('pages/page', {
+        menu: createMenuObject('cat'),
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg',
+        },
+        list,
+    });
+};
+
+export const fishes = (req: Request, res: Response) => {
+    // ► Filtragem → (Models)
+    let list = Pet.getFromType('fish');
+
+    res.render('pages/page', {
+        menu: createMenuObject('fish'),
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg',
+        },
+        list,
+    });
+};
